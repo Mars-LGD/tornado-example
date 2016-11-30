0c55e813bf8af05b12f10f47cd908c15412c347a
LiChenXiao(桌面事业部_号码通部) <lichenxiao@sogou-inc.com>
2016-11-30 10:17:58 +0800

    add query_number_handler.

--- tornado_helloworld.py
+++ tornado_helloworld.py
@@ -5,15 +5,30 @@
 
 import tornado.ioloop
 import tornado.web
+import json
 
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("hello world")
 
+class QueryNumberHandler(tornado.web.RequestHandler):
+
+    def get(self,number):
+        print self.request.arguments
+        if not self.request.arguments.has_key('number'):
+            res = {}
+        else:
+            res = {}
+            res['number'] = self.request.arguments['number']
+            if len(res['number']) == 1 and self.request.arguments['number'] == ['10086']:
+                res['number'] = res['number'][0]
+                res['tag'] = '中国移动客服'
+        self.write(json.dumps(res))
 
 def make_app():
-    return tornado.web.Application([(r"/", MainHandler), ])
+    return tornado.web.Application([(r"/", MainHandler),
+                                    (r"/query_number?(.*)",QueryNumberHandler)])
 
 
 if __name__ == "__main__":
